5882b7bf7e2ddb9f2c49c4f8debf04ff99c4b143
karan <karan@test.com>
2025-09-01 14:15:41 +0530

    karan has commited this

--- src/test/java/tests/FourTest.java
+++ src/test/java/tests/FourTest.java
@@ -12,6 +12,7 @@ public class FourTest extends Base {
 	@Test
 		public void testFour() throws InterruptedException, IOException
 		{
+		System.out.println("Karan updated with this ###");
 			System.out.println("test FOUR---");
 			 driver= initializeDriver();
 			driver.get("https://tutorialsninja.com/demo/");
